58a90d1bbad50c842036f188147920f53b0e4af1
Michael Cullimore <michaelcullimore@mail.weber.edu>
2017-03-29 22:33:22 -0600

    figure out how to initialize an array list again. I'm really starting from scatch

--- src/chat/Student.java
+++ src/chat/Student.java
@@ -8,4 +8,12 @@ public class Student {
     private double score;
 
     ArrayList<String> conversations;
+
+    // create a student
+    public Student(String firstN, String lastN, double _score, String[] convos) {
+	this.firstName = firstN;
+	this.lastName = lastN;
+	this.score = _score;
+	this.conversations = convos;// how do I initialize this???
+    }
 }
