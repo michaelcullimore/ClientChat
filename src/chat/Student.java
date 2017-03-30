6871d816204e6d84cef01549962da8789d4a3df3
Michael Cullimore <michaelcullimore@mail.weber.edu>
2017-03-29 22:25:23 -0600

    initial commit

---
+++ src/chat/Student.java
@@ -0,0 +1,11 @@
+package chat;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private double score;
+
+    ArrayList<String> conversations;
+}
