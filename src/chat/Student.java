faebfff3073abef6f9570cdd429fd9cb532d1c52
Michael Cullimore <michaelcullimore@mail.weber.edu>
2017-03-30 19:24:19 -0600

    Don't know what just happened, but I'll see if I can fix it

--- src/chat/Student.java
+++ src/chat/Student.java
@@ -4,8 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Student {
+    public static void main(String[] args) {
+	// make a test conversation, create a student
+	String[] testconvo = { "test", "hi", "Guten Morgen!", "Hey", "g2g" };
+	Student student01 = new Student("Michael", "Cullimore", 97.5, testconvo);
+
+	// test
+	System.out.println(student01.toString());
+	System.out.println(student01.conversations.get(0));
+    }
+
     private String firstName;
     private String lastName;
+
     private double score;
 
     ArrayList<String> conversations;
@@ -16,6 +27,27 @@ public class Student {
 	this.lastName = lastN;
 	this.score = _score;
 	this.conversations = new ArrayList<>(Arrays.asList(convos));
-
     }
+
+    public String getFirstName(String firstName) {
+	return firstName;
+    }
+
+    public String getLastName(String lastName) {
+	return lastName;
+    }
+
+    public double getScore(double score) {
+	return score;
+    }
+
+    public void setScore(double score) {
+	this.score = score;
+    }
+
+    @Override
+    public String toString() {
+	return firstName + " " + lastName + ": " + score + ", " + conversations + "}";
+    }
+
 }
